506588e2dfc5644f5727db220b4d8bba555b536f
vssamen7 <vssamen7@gmail.com>
2025-05-11 22:43:14 +0300

    Initial Commit

---
+++ laba5s/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  laba5s
+//
+//  Created by Иван Алейник on 11.05.25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
